2164c65407e29949329753577726c087817bce50
Gerlopak <elweydelpanxd.com>
2024-08-22 17:55:13 -0500

    cambio de codigo e implementacion de la continuacion

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,47 @@
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
         System.out.println("Ingrese la cantidad que desea apostar");
         int apos = scanner.nextInt();
         int sur;
-        int i=1;
+        int i = 1;
         do {
-            System.out.println("partida numero "+i);
+            System.out.println("Partida número " + i);
             sur = random.nextInt(1,4);
-            if(sur==1) {
-                System.out.println("¡Felicidades!,ganaste el doble de lo que apostaste");
+            if (sur == 1) {
+                System.out.println("¡Felicidades!, ganaste el doble de lo que apostaste.");
                 apos = apos * 2;
-                System.out.println("saldo actual " + apos);
-                System.out.println("   ");
-                i++;
-                Thread.sleep(2000);
-                //se implemento la linea de codigo Thread.sleep(); para realizar una pausa
-                //en este caso de dos segundos pero ya que trabaja con milisegundos se coloca 2000
-                // tambien por correcion del programa se agrego la linea
-                //"throws InterruptedException" para su correcto funcionamiento
+                System.out.println("Saldo actual: " + apos);
+            } else if (sur == 2) {
+                System.out.println("¡Oops!, perdiste la mitad de lo que apostaste.");
+                apos = apos / 2;
+                System.out.println("Saldo actual: " + apos);
+            } else if (sur == 3) {
+                System.out.println("Lo siento, perdiste todo.");
+                apos = 0;
+                System.out.println("Saldo actual: " + apos);
+                System.out.println("Gracias por jugar.");
+                break;
             }
-            else if(sur==2){
-                System.out.println("¡opps!,perdiste la mitad de lo que apostaste");
-                apos=apos/2;
-               System.out.println("saldo actual "+apos);
-                System.out.println("   ");
-                i++;
-               Thread.sleep(2000);
+            if (apos > 0) {
+                System.out.println("¿Quiere seguir apostando? (Y/N)");
+                String con = scanner.next();
+                if (con.equalsIgnoreCase("N")) {
+                    System.out.println("Gracias por jugar. Que tenga un buen día.");
+                    break;
+                } else {
+                    System.out.println("Perfecto, sigamos jugando.");
+                }
+            } else {
+                System.out.println("No tienes saldo suficiente para seguir apostando.");
+                break;
             }
-            else if(sur==3){
-                System.out.println("lo siento perdite todo");
-                apos=0;
-                System.out.println("saldo actual "+apos);
-                System.out.println("gracias por jugar");
-                System.out.println("   ");
-                i++;
-                Thread.sleep(2000);
-
-            }
-        }while(sur!=3);
+            i++;
+        } while (true);
+        scanner.close();
     }
 }
